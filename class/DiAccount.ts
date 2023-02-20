b90a7f3200e14a924e73809a070a5aa6dca19094
dilsoncampelo10 <dilsono316@gmail.com>
2023-02-20 17:35:53 -0300

    ajustando setup

--- class/DiAccount.ts
+++ class/DiAccount.ts
@@ -1,13 +1,20 @@
 export abstract class DiAccount{
+
     private number:number;
     private name:string;
+    private balance:number;
 
     constructor(number:number,name:string){
         this.number = number;
         this.name = name;
+        this.balance = 0
     }
 
     deposit = (value:number) => {
         
+    }
+
+    withdraw = (value:number) => {
+
     }
 }
